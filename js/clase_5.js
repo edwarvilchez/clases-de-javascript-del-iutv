a07282d9625b0ca8347f957e0146ceb4076712c4
edwarvilchez <edwarvilchez1977@gmail.com>
2021-08-07 19:12:25 -0400

    Se agregan cambios en el archivo js/clase_5.js

---
+++ js/clase_5.js
@@ -0,0 +1,23 @@
+// cadena de texto
+
+let text = "John Doe";
+
+let nro = "123456";
+
+let carName1 = "Volvo XC60";  // Double quotes
+let carName2 = 'Volvo XC60';  // Single quotes
+
+// let answer = "It's alright";
+/*let answer = "It is alright";*/
+
+let answer = "It's alright";
+console.log(answer);
+
+let answer2 = 'It"s" alright'
+answer2.length;
+console.log(answer2);
+
+
+
+// let nombreApellido
+
